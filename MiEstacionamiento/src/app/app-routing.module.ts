988f6dd79d4d84d140ac4006762bb1eee236cf23
TitoD01 <r.diazh2002@icloud.com>
2023-11-08 14:04:03 -0300

    paginas

--- MiEstacionamiento/src/app/app-routing.module.ts
+++ MiEstacionamiento/src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'registro',
+    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
 ];
 
 @NgModule({
