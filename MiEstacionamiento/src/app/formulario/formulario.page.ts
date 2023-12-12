5b9d010a1cab63bded8d8b2727d3bb01aeb8f35b
TitoD01 <137013698+TitoD01@users.noreply.github.com>
2023-12-12 16:56:28 -0300

    formulario agregar tarjeta

--- MiEstacionamiento/src/app/formulario/formulario.page.ts
+++ MiEstacionamiento/src/app/formulario/formulario.page.ts
@@ -16,25 +16,6 @@ export class FormularioPage {
 
   constructor(private router: Router, private alertController: AlertController) {}
 
-  realizarPago(tipoPago: string) {
-    if (tipoPago === 'apple-pay') {
-      // Lógica para Apple Pay
-    } else if (tipoPago === 'paypal') {
-      window.location.href = 'https://www.paypal.com/invoice/p/#INV2-FBZT-C4ZL-BCHT-BLV7';
-    } else if (tipoPago === 'tarjeta') {
-      // Validaciones de tarjeta de crédito
-      if (!this.validarTarjeta()) {
-        this.mostrarAlerta('Error', 'Por favor, completa los detalles de la tarjeta correctamente.');
-        return;
-      }
-
-      // Lógica de procesamiento de pago
-
-      // Muestra un cuadro de diálogo con "Pago realizado con éxito"
-      this.mostrarAlerta('Éxito', 'Pago realizado con éxito');
-    }
-  }
-
   private validarTarjeta(): boolean {
     const numTarjetaRegex = /^\d{16}$/;
     const fechaExpiracionRegex = /^(0[1-9]|1[0-2])\/\d{2}$/;
@@ -58,13 +39,11 @@ export class FormularioPage {
     await alert.present();
   }
   formatoFechaExpiracion(event: any) {
-    // Verifica si el evento es una tecla de borrar y borra todos los números de la fecha
     if (event.inputType === 'deleteContentBackward') {
       this.fechaExpiracion = '';
       return;
     }
   
-    // Resto de la lógica para dar formato a la fecha
     this.fechaExpiracion = this.fechaExpiracion.replace(/[^\d]/g, '');
   
     if (this.fechaExpiracion.length > 4) {
@@ -76,16 +55,12 @@ export class FormularioPage {
     }
   }
   formatoNumeroTarjeta() {
-    // Elimina caracteres no numéricos
     this.numeroTarjeta = this.numeroTarjeta.replace(/\D/g, '');
   
-    // Limita a 16 dígitos
     if (this.numeroTarjeta.length > 16) {
       this.numeroTarjeta = this.numeroTarjeta.slice(0, 16);
     }
   
-    // Separa cada 4 dígitos con un espacio
-    //this.numeroTarjeta = this.numeroTarjeta.replace(/(\d{4})/g, '$1 ').trim();
   }
   
 
