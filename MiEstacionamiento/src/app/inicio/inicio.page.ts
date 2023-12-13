39ed1e9061df078c4ef895c984f1e847286ffed2
TitoD01 <137013698+TitoD01@users.noreply.github.com>
2023-12-13 01:15:53 -0300

    valisaciones(dueño, cliente,regestacionamiento,inicio)

--- MiEstacionamiento/src/app/inicio/inicio.page.ts
+++ MiEstacionamiento/src/app/inicio/inicio.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { DataService } from '../data.service';
-
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-inicio',
@@ -13,17 +13,22 @@ export class InicioPage implements OnInit {
   comuna: string = '';
   searchResults: any[] = [];
   welcomeMessage: string = '';
+  comunaNotFoundError: boolean = false;
 
-  constructor(private authService: AuthService, private router: Router, private dataService: DataService) {
-  }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private dataService: DataService,
+    private alertController: AlertController
+  ) {}
 
   ngOnInit() {
     const user = this.authService.getCurrentUser();
     console.log('Información del usuario en inicio:', user);
-  
+
     // Almacena la información del usuario en el servicio AuthService
     this.authService.setUser(user, this.authService.getTipoUsuarioLocalStorage());
-  
+
     if (user && user.nombre_cli && user.apellido_cli) {
       this.welcomeMessage = `${user.nombre_cli} ${user.apellido_cli}`;
     } else if (user && user.nombre_dueno && user.apellido_dueno) {
@@ -31,10 +36,28 @@ export class InicioPage implements OnInit {
     }
   }
 
+  async presentComunaNotFoundErrorAlert() {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: 'La comuna que estás buscando no fue encontrada. Por favor, verifica el nombre e intenta nuevamente.',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
   search() {
     this.authService.searchByComuna(this.comuna).subscribe(
       (results) => {
         this.searchResults = results;
+
+        // Verificar si no se encontraron resultados
+        this.comunaNotFoundError = this.searchResults.length === 0;
+
+        // Mostrar la alerta si no se encuentra la comuna
+        if (this.comunaNotFoundError) {
+          this.presentComunaNotFoundErrorAlert();
+        }
       },
       (error) => {
         console.error('Error en la búsqueda:', error);
@@ -46,12 +69,11 @@ export class InicioPage implements OnInit {
     // Usa el servicio para compartir datos
     this.dataService.changeEstacionamiento(estacionamiento);
     this.router.navigate(['/arriendo']);
-}
+  }
 
-logout() {
-  // Implementa la lógica para cerrar la sesión aquí
-  this.authService.setUser(null, null); // Establecer ambos valores como null
-  this.router.navigate(['/home']);
-}
-
+  logout() {
+    // Implementa la lógica para cerrar la sesión aquí
+    this.authService.setUser(null, null); // Establecer ambos valores como null
+    this.router.navigate(['/home']);
+  }
 }
